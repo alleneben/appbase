30e7cff8f0b8eefaecacb4d4a01efb9e42586be1
Allen <alleneben@gmail.com>
2019-10-20 10:09:15 -0400

    Creating the dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import styles from './assets/styles/styles.module.scss';
+
+
 function App() {
   return (
-    <div className="App">
-      App
+    <div className={styles.app}>
+      <div className={styles.navline}></div>
+      <nav className={styles.nav}>
+        <h1>logo</h1>
+        <ul>
+          <li>Dashboard</li>
+          <li>New Member</li>
+          <li>Set up</li>
+          <li>Finance</li>
+          <li>Reports</li>
+        </ul>
+      </nav>
+
+      <div className={styles.main}>
+        <div className={styles.sidemenu}>
+          hdh
+        </div>
+        <div className={styles.content}>
+          
+          <div className={styles.card}>
+            content
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
