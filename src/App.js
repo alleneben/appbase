7991eae183b0b741ed0705bdcc25901d6d6cc08d
Allen <alleneben@gmail.com>
2019-10-21 07:15:48 -0400

    Creating dashboard

--- src/App.js
+++ src/App.js
@@ -21,14 +21,17 @@ function App() {
       <div className={styles.main}>
         <div className={styles.sidemenu}>
           <ul>
-            <li><i className="fa fa-home"></i>Affiliates</li>
-            <li><i className="fa fa-envelope"></i>hello</li>
-            <li><i className="fa fa-truck"></i>hii</li>
-            <li><i className="fa fa-tags"></i>hello</li>
-            <li><i className="fa fa-users"></i>hello</li>
-            <li><i className="fa fa-truck"></i>hii</li>
-            <li><i className="fa fa-tags"></i>hello</li>
-            <li><i className="fa fa-users"></i>hello</li>
+            <li><i className="fa fa-home"></i><span>Affiliates</span></li>
+            <li><i className="far fa-envelope"></i><span>Email Servers</span></li>
+            <li><i className="fa fa-truck"></i><span>Fulfillment</span></li>
+            <li><i className="fa fa-tags"></i><span>Gateways</span></li>
+            <li><i className="fa fa-users"></i><span>Integrations</span></li>
+            <li><i className="fa fa-truck"></i><span>Merchants</span></li>
+            <li><i className="fa fa-tags"></i><span>Products</span></li>
+          </ul>
+          <p>TOOLS</p>
+          <ul>
+            <li className={styles.iso}><i className="fas fa-life-ring"></i>Decline Salvage</li>
           </ul>
         </div>
         <div className={styles.content}>
